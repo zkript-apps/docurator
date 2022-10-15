f7a6cc864014145dcb849fe96edf5db838a5be92
John Patrick Madrigal <jp.madrigal07@gmail.com>
2022-10-15 18:35:09 +0800

    chore: removed .history folder

--- .history/src/utils/mongodb_20221015132618.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose'
-import { mongoURL } from '../config'
-
-mongoose
-  .connect(mongoURL)
-  .then(() => console.log('🚀 MONGODB is running'))
-  .catch((err) => console.log('❌ MONGODB error: ' + err))
--- .history/src/utils/mongodb_20221015135656.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-import { mongoURL } from '../config'
-
-console.log(mongoURL)
-mongoose
-  .connect(mongoURL)
-  .then(() => console.log('🚀 MONGODB is running'))
-  .catch((err) => console.log('❌ MONGODB error: ' + err))
