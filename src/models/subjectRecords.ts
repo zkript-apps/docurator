570aff8a65355bdfee7d6553f94230199ce9b7ef
John Patrick Madrigal <jp.madrigal07@gmail.com>
2022-10-17 21:37:00 +0800

    feat: added subjectRecord routes

---
+++ src/models/subjectRecords.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+const { Schema } = mongoose
+
+const subjectRecords = new Schema({
+  studentId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Students',
+    required: true,
+  },
+  subjectName: {
+    type: String,
+    required: true,
+  },
+  subjectCode: String,
+  firstGrading: Number,
+  secondGrading: Number,
+  thirdGrading: Number,
+  fourthGrading: Number,
+  finalGrade: Number,
+  remarks: String,
+  gradeLevel: {
+    type: String,
+    enum: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: Date,
+  deletedAt: Date,
+})
+
+module.exports = mongoose.model('SubjectRecords', subjectRecords)
