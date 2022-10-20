96c0bc8f8dc390ee79aa829b68a66fe85c5ca242
John Patrick Madrigal <jp.madrigal07@gmail.com>
2022-10-20 15:48:44 +0800

    chore: fixed daysOfSchool model

--- src/models/attendances.ts
+++ src/models/attendances.ts
@@ -13,6 +13,16 @@ const attendances = new Schema({
     minlength: 12,
     maxlength: 12,
   },
+  academicYear: {
+    from: String,
+    to: String,
+    required: true,
+  },
+  gradeLevel: {
+    type: String,
+    enum: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
