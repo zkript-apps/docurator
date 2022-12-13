aae7b97ddffecc449bc5435dd2a895ee9d094584
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2022-12-13 22:36:26 +0800

    feat: added form 138 custom get

--- src/routes/form138/index.ts
+++ src/routes/form138/index.ts
@@ -1,16 +1,20 @@
 import express from 'express'
 const router = express.Router()
-const {
+import {
   getAllForm138,
   addForm138,
   updateForm138,
   deleteForm138,
-} = require('./default')
+} from './default'
+import { getAllForm138WithAccess } from './customGet'
 import { isUserLoggedIn } from '../../../helper'
 
 router.get('/', isUserLoggedIn, getAllForm138)
 router.post('/', isUserLoggedIn, addForm138)
 router.patch('/:id', isUserLoggedIn, updateForm138)
 router.delete('/:id', isUserLoggedIn, deleteForm138)
+
+//custom get
+router.get('/with-access', isUserLoggedIn, getAllForm138WithAccess)
 
 module.exports = router
