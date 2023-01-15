4ce3eba05deda48b7563721a609b28e16c3dec2e
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-01-15 16:22:32 +0800

    fix: added access control headers

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express, { Application } from 'express'
+import cors from 'cors'
 import { port, origins } from './config'
 import routes from './routes'
 import './utils/mongodb'
 
 const app: Application = express()
 app.use(express.json())
+app.use(cors())
 routes(app)
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`🚀 SERVER is running at ${port}`)
