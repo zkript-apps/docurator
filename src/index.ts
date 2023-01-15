a1288fa52c2fce1d0f1c32dcbddb40520847e817
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-01-15 16:19:31 +0800

    fix: disabled cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express, { Application } from 'express'
-import cors from 'cors'
 import { port, origins } from './config'
 import routes from './routes'
 import './utils/mongodb'
 
 const app: Application = express()
 app.use(express.json())
-app.use(cors())
 routes(app)
 
 app.listen(port, () => {
