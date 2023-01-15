b09a96e7c42d46047fb31e62e05010e1c488b864
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-01-15 16:58:46 +0800

    fix: cors error

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,13 @@ import './utils/mongodb'
 
 const app: Application = express()
 app.use(express.json())
-// app.use(
-//   cors({
-//     origin: origins,
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-//   })
-// )
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
+app.use(
+  cors({
+    origin: origins,
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  })
+)
 routes(app)
 
 app.listen(port, () => {
